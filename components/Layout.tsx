54253c69fee5a2e49e2ac26d07abfa41718ddbb3
mstrueby <122125717+mstrueby@users.noreply.github.com>
2023-09-25 12:53:12 +0000

    min-h-screen

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,7 +12,7 @@ export default function Layout({
   return (
     <div className="">
       <Header />
-      <main className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <main className="min-h-screen mx-auto max-w-7xl sm:px-6 lg:px-8">
         {children}
       </main>
       <Footer />
