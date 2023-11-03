2c57c15bdc94cb36e020541e7aebf6d7736272fd
mstrueby <122125717+mstrueby@users.noreply.github.com>
2023-11-03 22:25:03 +0000

    matches / standings tabs

--- pages/tournaments/[alias].tsx
+++ pages/tournaments/[alias].tsx
@@ -67,7 +67,9 @@ export default function Tournament({
   tournament: Tournament
 }) {
 
-  let seasons: Season[] = tournament ? tournament.seasons.sort((a, b) => a.year - b.year) : [];
+  const [activeTab, setActiveTab] = useState('matches');
+
+  let seasons: Season[] = tournament ? tournament.seasons.sort((a, b) => b.year - a.year) : [];
   const [selectedSeason, setSelectedSeason] = useState(seasons ? seasons[0] : {} as Season);
 
   let rounds: Round[] = selectedSeason ? selectedSeason.rounds.sort((a, b) => new Date(a.start_date).getTime() - new Date(b.start_date).getTime()) : [];
@@ -81,28 +83,31 @@ export default function Tournament({
     setSelectedSeason(seasons.reduce((prev, current) => (prev.year > current.year) ? prev : current));
     setSelectedRound(rounds ? rounds[rounds.length - 1] : {} as Round);
     setSelectedMatchday(matchdays ? matchdays[matchdays.length - 1] : {} as Matchday);
+    setActiveTab('matches');
   }, [tournament]);
 
   useEffect(() => {
     setSelectedRound(rounds ? rounds[rounds.length - 1] : {} as Round);
+    setActiveTab('matches');
   }, [selectedSeason]);
 
   useEffect(() => {
     setSelectedMatchday(matchdays ? matchdays[matchdays.length - 1] : {} as Matchday)
+    setActiveTab('matches');
   }, [selectedRound]);
 
   const tabs = [
-    { name: 'Spiele', href: '#', current: true },
-    { name: 'Tabelle', href: '#', current: false },
+    { key: 'matches', caption: 'Spiele', href: '' },
+    { key: 'standings', caption: 'Tabelle', href: '' },
   ]
   function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
   }
 
   function formatDate(date_from: Date, date_to: Date) {
-    return `${format(new Date(date_from), 'd. LLL')}${ (new Date(date_to) > new Date(date_from)) ? " - " + format(new Date(date_to), 'd. LLL') : "" }`
+    return `${format(new Date(date_from), 'd. LLL')}${(new Date(date_to) > new Date(date_from)) ? " - " + format(new Date(date_to), 'd. LLL') : ""}`
   }
-  
+
   const router = useRouter()
 
   // If the page is not yet generated, this will be displayed
@@ -126,6 +131,8 @@ export default function Tournament({
         </div>
 
         <div className="mt-4 flex sm:ml-4 sm:mt-0 justify-end">
+
+          {/* Drop-Down SEASON */}
           <Listbox value={selectedSeason} onChange={setSelectedSeason}>
             {({ open }) => (
               <>
@@ -183,7 +190,8 @@ export default function Tournament({
               </>
             )}
           </Listbox>
-          
+
+          {/* Drop-Down ROUND */}
           {rounds.length > 1 &&
             <Listbox value={selectedRound} onChange={setSelectedRound}>
               {({ open }) => (
@@ -200,7 +208,7 @@ export default function Tournament({
                         <ChevronDownIcon className="h-5 w-5 text-white" aria-hidden="true" />
                       </Listbox.Button>
                     </div>
-  
+
                     <Transition
                       show={open}
                       as={Fragment}
@@ -255,19 +263,24 @@ export default function Tournament({
 
           <div className="sm:block ">
             <nav className="-mb-px flex space-x-8">
-              {tabs.map((tab) => (
+              {tabs.map((tab, index) => (
                 <a
-                  key={tab.name}
+                  key={index}
                   href={tab.href}
                   className={classNames(
-                    tab.current
+                    tab.key == activeTab
                       ? 'border-indigo-500 text-indigo-600'
                       : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
                     'whitespace-nowrap border-b-2 px-1 pb-4 text-sm font-medium'
+                    , tab.key === 'standings' && selectedRound.create_standings === false ? 'hidden' : ''
                   )}
-                  aria-current={tab.current ? 'page' : undefined}
+                  aria-current={tab.key == activeTab ? 'page' : undefined}
+                  onClick={(event) => {
+                    event.preventDefault();
+                    setActiveTab(tab.key)
+                  }}
                 >
-                  {tab.name}
+                  {tab.caption}
                 </a>
               ))}
             </nav>
@@ -275,96 +288,108 @@ export default function Tournament({
         </div>
       </div>
 
-      <section>
-        {matchdays.length > 1 &&
-          <Listbox value={selectedMatchday} onChange={setSelectedMatchday}>
-            {({ open }) => (
-              <>
-                <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">Spieltag:</Listbox.Label>
-                <Listbox.Label className="sr-only">Change Matchday</Listbox.Label>
-                <div className="relative mt-2">
-                  <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
-                    <span className="inline-flex w-full truncate">
-                      <span className="truncate">{selectedMatchday.name}</span>
-                      <span className="ml-2 truncate text-gray-500">{formatDate(selectedMatchday.start_date, selectedMatchday.end_date)}</span>
-                    </span>
-                    <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                      <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                    </span>
-                  </Listbox.Button>
 
-                  <Transition
-                    show={open}
-                    as={Fragment}
-                    leave="transition ease-in duration-100"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                      {matchdays.map((matchday, index) => (
-                        <Listbox.Option
-                          key={index}
-                          className={({ active }) =>
-                            classNames(
-                              active ? 'bg-indigo-600 text-white' : 'text-gray-900',
-                              'relative cursor-default select-none py-2 pl-3 pr-9'
-                            )
-                          }
-                          value={matchday}
-                        >
-                          {({ selected, active }) => (
-                            <>
-                              <div className="flex">
-                                <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'truncate')}>
-                                  {matchday.name}
-                                </span>
-                                <span className={classNames(active ? 'text-indigo-200' : 'text-gray-500', 'ml-2 truncate')}>
-                                  {formatDate(matchday.start_date, matchday.end_date)}
-                                </span>
-                              </div>
+      {activeTab == 'matches' && (
+        <section>
+          {matchdays.length > 1 &&
+            <Listbox value={selectedMatchday} onChange={setSelectedMatchday}>
+              {({ open }) => (
+                <>
+                  <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">Spieltag:</Listbox.Label>
+                  <Listbox.Label className="sr-only">Change Matchday</Listbox.Label>
+                  <div className="relative mt-2">
+                    <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+                      <span className="inline-flex w-full truncate">
+                        <span className="truncate">{selectedMatchday.name}</span>
+                        <span className="ml-2 truncate text-gray-500">{formatDate(selectedMatchday.start_date, selectedMatchday.end_date)}</span>
+                      </span>
+                      <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                        <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                      </span>
+                    </Listbox.Button>
 
-                              {selected ? (
-                                <span
-                                  className={classNames(
-                                    active ? 'text-white' : 'text-indigo-600',
-                                    'absolute inset-y-0 right-0 flex items-center pr-4'
-                                  )}
-                                >
-                                  <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                </span>
-                              ) : null}
-                            </>
-                          )}
-                        </Listbox.Option>
-                      ))}
-                    </Listbox.Options>
-                  </Transition>
+                    <Transition
+                      show={open}
+                      as={Fragment}
+                      leave="transition ease-in duration-100"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                        {matchdays.map((matchday, index) => (
+                          <Listbox.Option
+                            key={index}
+                            className={({ active }) =>
+                              classNames(
+                                active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                                'relative cursor-default select-none py-2 pl-3 pr-9'
+                              )
+                            }
+                            value={matchday}
+                          >
+                            {({ selected, active }) => (
+                              <>
+                                <div className="flex">
+                                  <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'truncate')}>
+                                    {matchday.name}
+                                  </span>
+                                  <span className={classNames(active ? 'text-indigo-200' : 'text-gray-500', 'ml-2 truncate')}>
+                                    {formatDate(matchday.start_date, matchday.end_date)}
+                                  </span>
+                                </div>
+
+                                {selected ? (
+                                  <span
+                                    className={classNames(
+                                      active ? 'text-white' : 'text-indigo-600',
+                                      'absolute inset-y-0 right-0 flex items-center pr-4'
+                                    )}
+                                  >
+                                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                  </span>
+                                ) : null}
+                              </>
+                            )}
+                          </Listbox.Option>
+                        ))}
+                      </Listbox.Options>
+                    </Transition>
+                  </div>
+                </>
+              )}
+            </Listbox>
+          }
+
+          {selectedMatchday.matches?.map((match, index) => (
+            <div key={index} className="flex justify-between gap-x-6 p-4 mt-4 mb-10 border rounded-xl">
+              <div className="flex gap-x-4">
+                <div className="min-w-0 flex-auto">
+                  <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src="" alt="" />
+                  <p className="text-sm font-semibold leading-6 text-gray-900">{match.home_team}</p>
+                  <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src="" alt="" />
+                  <p className="text-sm font-semibold leading-6 text-gray-900">{match.away_team}</p>
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">{match.venue},&nbsp;
+                    <time dateTime={match.start_time}>{format(new Date(match.start_time), 'd. MMM yy')}</time>
+                  </p>
                 </div>
-              </>
-            )}
-          </Listbox>
-        }
-
-        {selectedMatchday.matches?.map((match, index) => (
-          <div key={index} className="flex justify-between gap-x-6 p-4 mt-4 mb-10 border rounded-xl">
-            <div className="flex gap-x-4">
-              <div className="min-w-0 flex-auto">
-                <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src="" alt="" />
-                <p className="text-sm font-semibold leading-6 text-gray-900">{match.home_team}</p>
-                <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src="" alt="" />
-                <p className="text-sm font-semibold leading-6 text-gray-900">{match.away_team}</p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{match.venue},&nbsp;
-                  <time dateTime={match.start_time}>{format(new Date(match.start_time), 'd. MMM yy')}</time>
-                </p>
+              </div>
+              <div className="sm:flex sm:flex-col sm:items-end">
+                <p className="text-sm font-semibold leading-6 text-gray-900">{match.home_score}</p>
+                <p className="text-sm font-semibold leading-6 text-gray-900">{match.away_score}</p>
               </div>
             </div>
-            <div className="sm:flex sm:flex-col sm:items-end">
-              <p className="text-sm font-semibold leading-6 text-gray-900">{match.home_score}</p>
-              <p className="text-sm font-semibold leading-6 text-gray-900">{match.away_score}</p>
-            </div>
-          </div>
-        ))}
-      </section>
+          ))}
+        </section>
+      )}
+
+
+      {activeTab == 'standings' && (
+        <section className="mt-10">
+          <h2 className="text-2xl font-bold text-gray-900">Tabelle</h2>
+
+        </section>
+      )}
+
     </Layout>
   )
 }
