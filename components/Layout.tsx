246ac20a8e964fb100626cb268ebde9bb146b440
mstrueby <122125717+mstrueby@users.noreply.github.com>
2024-11-01 11:25:01 +0000

    stick footer to bottom of page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,12 +11,12 @@ export default function Layout({
 }) {
 
   return (
-    <div className="">
+    <div className="flex flex-col min-h-screen">
       <Head>
         <meta name="robots" content="noindex, nofollow" />
       </Head>
       <Header />
-      <main className="min-h-screen mx-auto max-w-7xl my-6 px-4 sm:px-6 lg:px-8">
+      <main className="flex-grow w-full mx-auto max-w-7xl my-6 px-4 sm:px-6 lg:px-8">
         {children}
       </main>
       <Footer />
