bff8417eb4e58e0dd5958dfa0b193ebce8b81bc5
MarianStruby <19449957-MarianStruby@users.noreply.replit.com>
2025-01-20 09:48:04 +0000

    User checkpoint: Refactor PlayerForm component: improved styling and removed legacy team display.

--- components/admin/PlayerFrom.tsx
+++ components/admin/PlayerFrom.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import InputText from '../ui/form/InputText';
-import { AutoAlias } from '../../tools/autoAlias';
 import ButtonPrimary from '../ui/form/ButtonPrimary';
 import ButtonLight from '../ui/form/ButtonLight';
 import { Switch } from '@headlessui/react';
-import MyListbox from '../ui/form/Listbox';
 import { PlayerValues } from '../../types/PlayerValues';
 import ImageUpload from '../ui/form/ImageUpload';
 import { CldImage } from 'next-cloudinary';
@@ -133,13 +131,13 @@ const PlayerForm: React.FC<PlayerFormProps> = ({
                               </div>
                             </div>
 
-                            <div className="relative gap-x-4 flex items-center">
+                            <div className="relative gap-x-8 flex items-center">
                               <input
                                 type="number"
                                 name={`assignedTeams.${index}.teams.${teamIndex}.jerseyNo`}
                                 min="1"
                                 max="98"
-                                className="ml-4 block w-16 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                                className="block w-16 rounded-md border-0 py-2 pl-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
                               />
                               <Switch
                                 checked={values.assignedTeams[index].teams[teamIndex].active || false}
@@ -157,29 +155,7 @@ const PlayerForm: React.FC<PlayerFormProps> = ({
                                   } pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out`}
                                 />
                               </Switch>
-                            </div>
-
-                            
-                          </li>
-                          /*
-                        <div key={teamIndex} className="mt-4 grid grid-cols-2 gap-4">
-                          <p>{team.teamName}</p>
-                          <p>{team.passNo} ({team.source})</p>
-                          <p>{new Date(team.modifyDate).toLocaleDateString('de-DE', {
-                            day: '2-digit',
-                            month: '2-digit',
-                            year: 'numeric',
-                          })}</p>
-                          <Toggle name={`teams.${teamIndex}.active`} type="checkbox" label="Aktiv" />
-                          <InputText
-                            name={`assignedTeams.${index}.teams.${teamIndex}.jerseyNo`}
-                            type="number"
-                            label="Trikotnummer"
-                            autoComplete="off"
-                          />
-                        </div>
-                        */
-
+                            </div></li>
                         ))}
                       </ul>
                     </div>
