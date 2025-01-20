d3ec5006d45c34222d329e486de5c4762b50ddd2
MarianStruby <19449957-MarianStruby@users.noreply.replit.com>
2025-01-20 09:24:57 +0000

    User checkpoint: Move jersey number input after team active toggle in PlayerForm.

--- components/admin/PlayerFrom.tsx
+++ components/admin/PlayerFrom.tsx
@@ -134,7 +134,6 @@ const PlayerForm: React.FC<PlayerFormProps> = ({
                             </div>
 
                             <div className="relative flex-none gap-x-4 flex items-center">
-                              <Toggle name={`teams.${teamIndex}.active`} type="checkbox" />
                               <input
                                 type="number"
                                 name={`assignedTeams.${index}.teams.${teamIndex}.jerseyNo`}
@@ -142,6 +141,7 @@ const PlayerForm: React.FC<PlayerFormProps> = ({
                                 max="98"
                                 className="ml-4 block w-16 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
                               />
+                              <Toggle name={`teams.${teamIndex}.active`} type="checkbox" />
                             </div>
 
                             
