45f407c8930b38ac7562ae6457ecff7cdad26ae0
MarianStruby <19449957-MarianStruby@users.noreply.replit.com>
2025-01-29 12:43:32 +0000

    User checkpoint: Improve middleware: redirect to www.bishl.de and add authentication check

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,25 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
+
+  const url = request.url; // corrected variable name
+  let cookie = request.cookies.get('jwt'); // corrected variable name
+
   const hostname = request.headers.get('host')
-  
+
   if (hostname === 'bishl.de') {
-    return NextResponse.redirect(`https://www.bishl.de${request.nextUrl.pathname}${request.nextUrl.search}`, 301)
+    return NextResponse.redirect(
+      `https://www.bishl.de${request.nextUrl.pathname}${request.nextUrl.search}`,
+      301
+    );
   }
   
-  return NextResponse.next()
+  if ((url.includes('/leaguemanager') || url.includes('/admin')) && !cookie) { // simplified condition
+    let loginUrl = `${process.env['NEXT_FRONTEND_URL']}/login`; // used template literals for consistency
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
