143389d9e322572e0a47706093428e153e187f43
mstrueby <122125717+mstrueby@users.noreply.github.com>
2025-03-07 14:18:38 +0000

    fix hinzufügen button style

--- pages/matches/[id]/roster/[teamFlag]/index.tsx
+++ pages/matches/[id]/roster/[teamFlag]/index.tsx
@@ -952,7 +952,7 @@ const RosterPage = ({ jwt, match, club, team, roster, rosterPublished: initialRo
                             type="button"
                             onClick={handleAddPlayer}
                             disabled={loading}
-                            className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 textsm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
                             <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
                             Hinzufügen
