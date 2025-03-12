925d7cb2fcd309f24265a1b05214fc49774c8871
mstrueby <122125717+mstrueby@users.noreply.github.com>
2025-03-12 10:43:05 +0000

    fix add team

--- components/ui/form/InputText.tsx
+++ components/ui/form/InputText.tsx
@@ -4,9 +4,10 @@ import type { ComponentPropsWithoutRef } from 'react';
 interface InputTextProps extends ComponentPropsWithoutRef<'input'> {
   label: string;
   name: string;
+  description?: string;
 }
 
-const InputText = ({ label, name, ...props }: InputTextProps) => {
+const InputText = ({ label, name, description, ...props }: InputTextProps) => {
   const [field, meta] = useField(name);
   const classInputDef = "block w-full rounded-md border-0 py-2 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 appearance-none"
   const classInputErr = "block w-full rounded-md border-0  py-2 pl-3 pr-10 text-red-900 ring-1 ring-inset ring-red-300 placeholder:text-red-300 focus:ring-2 focus:ring-inset focus:ring-red-500 sm:text-sm sm:leading-6 appearance-none"
@@ -17,6 +18,11 @@ const InputText = ({ label, name, ...props }: InputTextProps) => {
         className="block text-sm font-medium mt-6 mb-2 leading-6 text-gray-900">
         {label}
       </label>
+      {description && (
+        <p className="text-xs text-gray-500 mb-2">
+          {description}
+        </p>
+      )}
       <input
         className={meta.touched && meta.error ? classInputErr : classInputDef}
         {...field} {...props}
