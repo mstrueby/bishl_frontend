9c665e50386a4623318f1e8ddafe1b8b61f53647
mstrueby <122125717+mstrueby@users.noreply.github.com>
2025-03-26 15:01:43 +0000

    sort tournaments in select

--- components/ui/TournamentSelect.tsx
+++ components/ui/TournamentSelect.tsx
@@ -73,7 +73,10 @@ const TournamentSelect: React.FC<TournamentSelectProps> = ({
               leaveTo="opacity-0"
             >
               <Listbox.Options className="absolute z-50 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {allTournamentsData?.filter(tournament => tournamentConfigs[tournament.alias]?.active)?.map((tournament) => (
+                {allTournamentsData
+                  ?.filter(tournament => tournamentConfigs[tournament.alias]?.active)
+                  ?.sort((a, b) => (tournamentConfigs[a.alias]?.sortOrder || 0) - (tournamentConfigs[b.alias]?.sortOrder || 0))
+                  ?.map((tournament) => (
                   <Listbox.Option
                     key={tournament._id}
                     className={({ active }) =>
