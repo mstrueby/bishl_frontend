1d4db2340f8bb20c81e4afb175611e1270ce953e
mstrueby <122125717+mstrueby@users.noreply.github.com>
2025-07-17 09:10:40 +0000

    fix matchcenter buttons

--- pages/matches/[id]/matchcenter/index.tsx
+++ pages/matches/[id]/matchcenter/index.tsx
@@ -180,7 +180,7 @@ export default function MatchDetails({ match: initialMatch, matchdayOwner, jwt, 
   }, [id, isRefreshing]);
 
   const permissions = calculateMatchButtonPermissions(user, match, matchdayOwner, true);
-  
+
   const showButtonRosterHome = permissions.showButtonRosterHome;
   const showButtonRosterAway = permissions.showButtonRosterAway;
   const showButtonScoresHome = permissions.showButtonScoresHome;
@@ -234,7 +234,7 @@ export default function MatchDetails({ match: initialMatch, matchdayOwner, jwt, 
         </div>
 
         {/* Middle Section with Start/Finish Button */}
-        <div className="w-1/3 flex justify-center items-center">
+        <div className="w-1/3 flex justify-center items-center space-x-2">
           {showButtonStatus && new Date(match.startDate).getTime() < Date.now() + 30 * 60 * 1000 && (
             <>
               {match.matchStatus.key === 'SCHEDULED' && (
@@ -281,38 +281,32 @@ export default function MatchDetails({ match: initialMatch, matchdayOwner, jwt, 
                 </button>
               )}
 
-              {match.matchStatus.key === 'INPROGRESS' && showButtonStatus && (
-                <div className="flex space-x-2">
+              {showButtonStatus && (
+                <>
                   <button
                     onClick={() => setIsStatusDialogOpen(true)}
                     className="inline-flex items-center justify-center px-4 py-1.5 border border-transparent shadow-md text-sm font-medium rounded text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                   >
                     Status
                   </button>
-                  <button
-                    onClick={() => setIsFinishDialogOpen(true)}
-                    className="inline-flex items-center justify-center px-4 py-1.5 border border-transparent shadow-md text-sm font-medium rounded text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-                  >
-                    {isRefreshing ? (
-                      <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v2a6 6 0 00-6 6H4z"></path>
-                      </svg>
-                    ) : (
-                      'Beenden'
-                    )}
-                  </button>
-                </div>
+                  {match.matchStatus.key === 'INPROGRESS' && (
+                    <button
+                      onClick={() => setIsFinishDialogOpen(true)}
+                      className="inline-flex items-center justify-center px-4 py-1.5 border border-transparent shadow-md text-sm font-medium rounded text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                    >
+                      {isRefreshing ? (
+                        <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v2a6 6 0 00-6 6H4z"></path>
+                        </svg>
+                      ) : (
+                        'Beenden'
+                      )}
+                    </button>
+                  )}
+                </>
               )}
             </>
-          )}
-          {showButtonStatus && (match.matchStatus.key === 'FINISHED' || match.matchStatus.key === 'FORFEITED') && (
-            <button
-              onClick={() => setIsStatusDialogOpen(true)}
-              className="inline-flex items-center justify-center px-4 py-1.5 border border-transparent shadow-md text-sm font-medium rounded text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              Status
-            </button>
           )}
         </div>
 
