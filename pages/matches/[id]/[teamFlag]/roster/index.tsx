1397e80ea7d7e31558ad52b707bd9435b1cfa7a1
mstrueby <122125717+mstrueby@users.noreply.github.com>
2025-09-04 14:19:54 +0000

    tryied to fix tab order

--- pages/matches/[id]/[teamFlag]/roster/index.tsx
+++ pages/matches/[id]/[teamFlag]/roster/index.tsx
@@ -1902,14 +1902,14 @@ const RosterPage = ({ jwt, match, matchTeam, club, team, roster, rosterPublished
               <button
                 type="button"
                 onClick={handleCancelEdit}
-                className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                className="inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
                 Abbrechen
               </button>
               <button
                 type="button"
                 onClick={handleSaveEdit}
-                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="w-28 inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
               >
                 Speichern
               </button>
