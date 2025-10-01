116efae61c9b9effbb73522f177abcc890aff76a
mstrueby <122125717+mstrueby@users.noreply.github.com>
2025-10-01 09:33:31 +0000

    Restored to '746040d27310560cb429358db8f3adee3e0f9be6'
    Replit-Restored-To: 746040d27310560cb429358db8f3adee3e0f9be6

--- pages/matches/[id]/matchcenter/index.tsx
+++ pages/matches/[id]/matchcenter/index.tsx
@@ -818,7 +818,7 @@ export default function MatchDetails({
                   <h4 className="text-md font-medium text-gray-900 mb-4">
                     Dokumente / Ausrüstung
                   </h4>
-                  <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     {[
                       { key: "ruleBook", label: "Spielregeln/WKO" },
                       { key: "goalDisplay", label: "Manuelle Toranzeige" },
