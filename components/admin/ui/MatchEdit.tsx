3721aee6da61f9dda280bdca65466ecd2daf4a5d
mstrueby <122125717+mstrueby@users.noreply.github.com>
2026-04-16 08:52:19 +0000

    fix dialog overflow issue

--- components/admin/ui/MatchEdit.tsx
+++ components/admin/ui/MatchEdit.tsx
@@ -110,7 +110,7 @@ const MatchEdit = ({ isOpen, onClose, match, onSuccess, onMatchUpdate }: MatchEd
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+              <Dialog.Panel className="w-full max-w-md transform rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                 <Dialog.Title
                   as="h3"
                   className="text-lg text-center font-bold leading-6 text-gray-900 mb-4">
